0f13c462d4e1b5092023c6bdbfa516bd1f9a1431
abdullah <abdullahkhanorakzai@gmail.com>
2023-09-11 00:26:44 -0700

    second version

--- date.js
+++ date.js
@@ -5,12 +5,5 @@ exports.getDate = function () {
     month: "long",
     day: "numeric",
   };
-  return today.toLocaleDateString("en-US", options);
-};
-exports.getDay = function () {
-  const today = new Date();
-  const options = {
-    weekday: "long",
-  };
-  return today.toLocaleDateString("en-US", options);
+  return today.toLocaleDateString("ur-pk", options);
 };
